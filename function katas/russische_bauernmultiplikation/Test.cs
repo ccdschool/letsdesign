f37b2da87f406b6cd7adfe8f06366495b5fef328
Ralf Westphal <info@ralfw.de>
2016-03-04 17:10:35 +0100

    gerade zahlen finden

--- function katas/russische_bauernmultiplikation/Test.cs
+++ function katas/russische_bauernmultiplikation/Test.cs
@@ -11,24 +11,39 @@ namespace russische_bauernmultiplikation
 		[TestCase (2, 3, 6)]
 		public void TestMult (int a, int b, int erwartet)
 		{
-			var resultat = Mult (a, b);
+			var resultat = RussischeBauernmultiplikation.Mult (a, b);
 			Assert.AreEqual (erwartet, resultat);
 		}
 
 		[Test]
 		public void Test_Halbieren() {
-			var h = Halbieren (9);
+			var h = RussischeBauernmultiplikation.Halbieren (9);
 			Assert.That (h, Is.EqualTo (new[]{9, 4, 2, 1}));
 		}
 
 		[Test]
 		public void Test_Verdoppeln() {
-			var v = Verdoppeln (3, 4);
+			var v = RussischeBauernmultiplikation.Verdoppeln (3, 4);
 			Assert.That (v, Is.EqualTo (new[]{3, 6, 12, 24}));
 		}
 
+		[Test]
+		public void Test_Gerade_Zahlen_finden() {
+			var indizes = RussischeBauernmultiplikation.Gerade_Zahlen_finden (new[]{ 1, 2, 3, 4, 6 });
+			Assert.That (indizes, Is.EqualTo (new[]{1,3,4}));
+		}
 
-		int Mult(int a, int b) {
+
+		[Test]
+		public void Test_Streichen() {
+		
+		}
+	}
+
+
+
+	class RussischeBauernmultiplikation {
+		public static int Mult(int a, int b) {
 			var h = Halbieren (a).ToArray();
 			var v = Verdoppeln (b, h.Length);
 			var indizes = Gerade_Zahlen_finden (h);
@@ -37,14 +52,14 @@ namespace russische_bauernmultiplikation
 		}
 
 
-		IEnumerable<int> Halbieren(int i) {
+		public static IEnumerable<int> Halbieren(int i) {
 			while (i >= 1) {
 				yield return i;
 				i = i / 2;
 			}
 		}
 
-		IEnumerable<int> Verdoppeln(int i, int n) {
+		public static IEnumerable<int> Verdoppeln(int i, int n) {
 			while (n > 0) {
 				yield return i;
 				i *= 2;
@@ -52,11 +67,15 @@ namespace russische_bauernmultiplikation
 			}
 		}
 
-		IEnumerable<int> Gerade_Zahlen_finden(IEnumerable<int> zahlen) {
-			return null;
+		public static IEnumerable<int> Gerade_Zahlen_finden(IEnumerable<int> zahlen) {
+			var i = 0;
+			foreach (var z in zahlen) {
+				if (z % 2 == 0)	yield return i;
+				i++;
+			}
 		}
 
-		IEnumerable<int> Streichen(IEnumerable<int> zahlen, IEnumerable<int> indizes) {
+		public static IEnumerable<int> Streichen(IEnumerable<int> zahlen, IEnumerable<int> indizes) {
 			return null;
 		}
 	}
