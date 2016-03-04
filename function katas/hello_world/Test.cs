581e51c77a7f1f264f33df7ae220859a8a67fc6c
Ralf Westphal <info@ralfw.de>
2016-03-04 11:08:16 +0100

    grüner test für buzz zahlen

--- function katas/hello_world/Test.cs
+++ function katas/hello_world/Test.cs
@@ -11,14 +11,15 @@ namespace hello_world
 		[Test ()]
 		public void TestCase ()
 		{
-			var result = FizzBuzz (3);
-			Assert.That (result, Is.EqualTo (new[]{"1", "2", "Fizz"}));
+			var result = FizzBuzz (5);
+			Assert.That (result, Is.EqualTo (new[]{"1", "2", "Fizz", "4", "Buzz"}));
 		}
 
 
 		IEnumerable<string> FizzBuzz(int n) {
 			return Enumerable.Range (1, n).Select (i => {
 				if (i % 3 == 0) return "Fizz";
+				if (i % 5 == 0) return "Buzz";
 				return i.ToString();
 			});
 		}
