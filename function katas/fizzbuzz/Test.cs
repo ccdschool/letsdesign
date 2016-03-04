b17e45bd104053de775c9f4acbf4787adf4f0ad6
Ralf Westphal <info@ralfw.de>
2016-03-04 12:19:55 +0100

    fizzen und buzzen auch wenn 3 bzw 5 nur als ziffer in der zahl vorkommen

--- function katas/fizzbuzz/Test.cs
+++ function katas/fizzbuzz/Test.cs
@@ -13,7 +13,9 @@ namespace fizzbuzz
 		{
 			var resultate = FizzBuzz (1, 15);
 			Assert.That (resultate, Is.EqualTo (new[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
-													  "11", "Fizz", "13", "14", "FizzBuzz"}));
+													  "11", "Fizz", "Fizz", "14", "FizzBuzz"}));
+
+			Assert.That (FizzBuzz (58, 58), Is.EqualTo(new[]{"Buzz"}));
 		}
 
 
@@ -43,8 +45,8 @@ namespace fizzbuzz
 			return Klassen.Zahl;
 		}
 
-		static bool IsFizz (int i) => i % 3 == 0;
-		static bool IsBuzz (int i) => i % 5 == 0;
+		static bool IsFizz (int i) => i % 3 == 0 || i.ToString().IndexOf("3") >= 0;
+		static bool IsBuzz (int i) => i % 5 == 0 || i.ToString().IndexOf("5") >= 0;
 
 
 		string Übersetzen(Klassen klasse, int i) {
