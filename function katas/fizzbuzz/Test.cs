44087c3a0f5be2ba96bb90dcd23657a0a131d8af
Ralf Westphal <info@ralfw.de>
2016-03-04 11:54:05 +0100

    fizzbuzz erster test rot

---
+++ function katas/fizzbuzz/Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fizzbuzz
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			var resultate = FizzBuzz (1, 2);
+			Assert.That (resultate, Is.EqualTo (new[]{"1", "2"}));
+		}
+
+
+		IEnumerable<string> FizzBuzz(int start, int ende) {
+			for (var i = start; i <= ende; i++)
+				yield return i.ToString ();
+		}
+	}
+}
+
