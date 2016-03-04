0b136a67e8c8cc21c594feada1d5cef87993aa0c
Ralf Westphal <info@ralfw.de>
2016-03-04 11:09:44 +0100

    grün für fizzbuzz

--- function katas/hello_world/Test.cs
+++ function katas/hello_world/Test.cs
@@ -11,13 +11,15 @@ namespace hello_world
 		[Test ()]
 		public void TestCase ()
 		{
-			var result = FizzBuzz (5);
-			Assert.That (result, Is.EqualTo (new[]{"1", "2", "Fizz", "4", "Buzz"}));
+			var result = FizzBuzz (15);
+			Assert.That (result, Is.EqualTo (new[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", 
+												   "11", "Fizz", "13", "14", "FizzBuzz"}));
 		}
 
 
 		IEnumerable<string> FizzBuzz(int n) {
 			return Enumerable.Range (1, n).Select (i => {
+				if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
 				if (i % 3 == 0) return "Fizz";
 				if (i % 5 == 0) return "Buzz";
 				return i.ToString();
