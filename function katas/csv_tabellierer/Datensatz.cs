e3c33dc13a84620f6c7931fc896c1a04c4b49229
Ralf Westphal <info@ralfw.de>
2016-03-05 16:14:01 +0100

    refaktorisiert / gerüsttests gelöscht

---
+++ function katas/csv_tabellierer/Datensatz.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csv_tabellierer
+{
+
+	class Datensatz {
+		public string[] Werte;
+	}
+}
---
+++ function katas/csv_tabellierer/Tabellierer.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csv_tabellierer
+{
+
+	class Tabellierer {
+		public IEnumerable<string> Formatieren(IEnumerable<Datensatz> datensätze) {
+			var spaltenbreiten = Spaltenbreiten_bestimmen (datensätze);
+			var tabellenzeilen = Tabellenzeilen_bauen (datensätze, spaltenbreiten);
+			var unterstreichung = Unterstreichung_bauen (spaltenbreiten);
+			return Unterstreichung_einfügen (tabellenzeilen, unterstreichung);
+		}
+
+		int[] Spaltenbreiten_bestimmen(IEnumerable<Datensatz> datensätze) {
+			var spaltenbreiten = new int[datensätze.First().Werte.Length];
+			for (var iSpalte = 0; iSpalte < spaltenbreiten.Length; iSpalte++) {
+				spaltenbreiten[iSpalte] = datensätze.Select (ds => ds.Werte [iSpalte].Length).Max ();
+			}
+			return spaltenbreiten;
+		}
+
+		IEnumerable<string> Tabellenzeilen_bauen(IEnumerable<Datensatz> datensätze, int[] spaltenbreiten) {
+			return datensätze.Select (ds => {
+				var werteAufgefüllt = ds.Werte.Select((w,i) => w.PadRight(spaltenbreiten[i]));
+				return string.Join("|", werteAufgefüllt) + "|";
+			});
+		}
+
+		string Unterstreichung_bauen(int[] spaltenbreiten) {
+			var spaltenUnterstriche = spaltenbreiten.Select (sb => "".PadLeft(sb, '-')); // "-", "--", "---"
+			return string.Join("+", spaltenUnterstriche) + "+";
+		}
+
+		IEnumerable<string> Unterstreichung_einfügen(IEnumerable<string> tabellenzeilen, string unterstreichung) {
+			var tabListe = tabellenzeilen.ToList ();
+			tabListe.Insert (1, unterstreichung);
+			return tabListe;
+		}
+
+	}
+	
+}
