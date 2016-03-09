9b39322ed718f9f47e40e6895ded492b3748785f
Ralf Westphal <info@ralfw.de>
2016-03-09 16:21:39 +0100

    csvviewer init

---
+++ application katas/csvviewer/csvviewer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csvviewer
+{
+	// Beispieldatenquelle: https://www.destatis.de/DE/ZahlenFakten/LaenderRegionen/Regionales/Gemeindeverzeichnis/Administrativ/AdministrativeUebersicht.html
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
