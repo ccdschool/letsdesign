f3d5a01e90795f5a13d7a20a54079d50d5d21faa
Ralf Westphal <info@ralfw.de>
2016-03-10 17:30:08 +0100

    aktueller stand nach git fehler

--- README.md
+++
@@ -1,2 +0,0 @@
-# Let's Design
-Entwürf und Code der gleichnamigen Videoserie bei Youtube.
---
+++ application katas/csvviewer/csvviewer/App.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csvviewer
+{
+	// Beispieldatenquelle: https://www.destatis.de/DE/ZahlenFakten/LaenderRegionen/Regionales/Gemeindeverzeichnis/Administrativ/AdministrativeUebersicht.html
+
+	class App {
+		Blättern blättern;
+		csv_tabellierer.CSVTabellierer csvtab;
+		ConsolePortal con;
+		CsvCache cache;
+
+		public App(ConsolePortal con) {
+			this.blättern = new Blättern ();
+			this.csvtab = new csv_tabellierer.CSVTabellierer ();
+			this.con = con;
+
+			this.con.ErsteSeiteCmd += () => {
+				var tabellenzeilen = Erste_Seite_aufblättern (this.cache);
+				con.Tabelle_anzeigen (tabellenzeilen);
+			};
+		}
+
+		public void Run(string[] args) {
+			var cli = new CliPortal ();
+			var dat = new DateiProvider ();
+
+			var dateiname = cli.Starten (args);
+			var csvzeilen = dat.Datei_laden (dateiname);
+			this.cache = new CsvCache (csvzeilen);
+
+			var tabellenzeilen = Erste_Seite_aufblättern (this.cache);
+			con.Öffnen (tabellenzeilen,
+				() => {
+					var letzteTabellenzeilen = Letzte_Seite_aufblättern (this.cache);
+					con.Tabelle_anzeigen (letzteTabellenzeilen);	
+				});
+		}
+
+
+		IEnumerable<string> Erste_Seite_aufblättern(CsvCache cache) {
+			var csvseite = blättern.Erste_Seite_ermitteln (cache);
+			return csvtab.Tabellieren (csvseite.Zeilen);
+		}
+
+		IEnumerable<string> Letzte_Seite_aufblättern(CsvCache cache) {
+			var csvseite = blättern.Letzte_Seite_ermitteln (cache);
+			return csvtab.Tabellieren (csvseite.Zeilen);
+		}
+	}
+}
